6c3d7e72a40b79fac6536a6cfb6cfc32a196cb58
ibrahim-xaheer <ibrahim.xaheer@gmail.com>
2013-03-31 05:46:11 +0500

    refactored code 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,15 @@ class PagesController < ApplicationController
 
 
   def home
+  	@title = "Home"
   end
 
   def contact
+  	@title = "Contact"
   end
+  
+  def about
+  	@title = "About"
+  end
+  
 end
