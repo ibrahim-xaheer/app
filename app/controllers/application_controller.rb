415b1e1e930a953bd491039a3e95b2775eaa8408
ibrahim-xaheer <ibrahim.xaheer@gmail.com>
2013-03-31 16:39:44 +0500

    Added a title Helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  #return a title in a per-page basis.
+  def title
+  	base_title = "Facebook"
+  	if @title.nil?
+  		base_title
+  	else
+  		"#{base_title} | #{@title}"
+  	end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#return a title in a per-page basis.
+  def title
+  	base_title = "Facebook"
+  	if @title.nil?
+  		base_title
+  	else
+  		"#{base_title} | #{@title}"
+  	end
+  end
 end
