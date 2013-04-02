ff674f7a36e1692baac28df309d9252dd52a7ab7
ibrahim-xaheer <ibrahim.xaheer@gmail.com>
2013-04-03 03:11:15 +0500

    DB Validations & email index

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,13 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :fName, :lName
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z\d\-.]+\z/i
+
+  validates :fName,  presence: true, length: { maximum: 50 }
+  validates :lName,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, 
+  					format: { with: email_regex },
+                    uniqueness: true
+
 end
